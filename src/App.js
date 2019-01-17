2a22fd0a88de1c390fd4f481a7a5a1b72733e9b4
Albert <albert.hovhannisyan002@gmail.com>
2019-01-18 00:16:24 +0400

    added drag drop functionality fixed some errors

--- src/App.js
+++ src/App.js
@@ -4,57 +4,80 @@ import './App.css';
 class App extends Component {
   constructor(){
     super();
+
     this.state = {
       pictures:[],
-      buttonGroupes:[]
+      terms:[]
     }
-    //Getting images from flickr api
-    var self = this;
-    this.handleClick = function(){
-      var picArray = [];
-      var picNames = [];
-      var handleInput = document.getElementsByClassName("searchInput")[0].value.split(" ").length;
-      for(let i = 0; i < handleInput;++i){
-        fetch(  'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=f86d456db87838e53eea443b31630f4b&tags='+document.getElementsByClassName("searchInput")[0].value.split(" ")[i] +'&per_page=5&page=1&format=json&nojsoncallback=1'
-      )    
-      .then(function(response){
-        return response.json();
-      })
-      .then(function(j){
-        picArray.push(j.photos.photo.map((pic)=>{
-          var picPath = `https://farm${pic.farm}.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}.jpg`;
-          return(
-            <img alt="flick_img" src={picPath} className="image" draggable="true"/>
-          )
-        }));
-        picNames.push(document.getElementsByClassName("searchInput")[0].value.split(" ").map((name)=>{
-          return(
-            <input type="button" value={name} className="buttonGroupes"/>
-          )
-        }))
-        self.setState({pictures:picArray,buttonGroupes:picNames})
-      })
-      }
-    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.searchInput = null;
   }
-    
+
+  makePicPath(pic){
+    return `https://farm${pic.farm}.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}.jpg`;
+  }
+
+    dragStart(ev,term){
+      ev.dataTransfer.dropEffect = "move";
+      ev.dataTransfer.setData("text/plain",term);
+    }
+
+    dragOver(ev){
+      ev.preventDefault();
+      ev.dataTransfer.dropEffect = "move"
+    }
+    drop(ev){
+      ev.preventDefault();
+      console.log(ev.dataTransfer.getData("text/plain"))
+    }
+    handleSubmit(e){
+      let pictures = [];
+      let terms = this.searchInput.value.split(" ");
+      let doneRequests = 0;
+
+      e.preventDefault();
+      terms.forEach((term)=>{
+        fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=f86d456db87838e53eea443b31630f4b&tags='+term +'&per_page=5&page=1&format=json&nojsoncallback=1')
+        .then((res)=>res.json())
+        .then((resp)=>{
+          resp.photos.photo.forEach(pic=>{
+            let data = {};
+            pictures.push(
+              <img 
+              src={this.makePicPath(pic)}
+              alt={term}
+              key={pic.id}
+              onDragStart={$ev => this.dragStart($ev,term)}
+              draggable="true"
+            />
+            )
+          })
+          if(++doneRequests === terms.length){
+            this.setState({pictures:pictures,terms:terms})
+          }
+        })
+      })
+
+    }
+   
+  
   render() {
     return (
       <div className="App">
 
-          <div className="searchBar">
-            <input type="search" placeholder="Enter the keyword" className="searchInput"/>
+          <form className="searchBar" onSubmit={this.handleSubmit}>
+            <input type="search" placeholder="Enter the keyword" className="searchInput" ref={a => this.searchInput = a }/>
             <input type="submit" className="searchButton" value="Search" onClick={this.handleClick}/>
-          </div><br/>
+          </form>
 
           <div className="images">
               {this.state.pictures}
           </div>
 
           <div className="groupChoose">
-          {/* why when i remove this line of code buttons multipies? */}
-           {console.log()} 
-           {this.state.buttonGroupes}
+            {this.state.terms.map(term =>
+                <div className="term" key={term} onDragOver={this.dragOver} onDrop={this.drop}>{term}</div>
+            )}
           </div>
 
           <div className="groupPics">
@@ -63,7 +86,9 @@ class App extends Component {
       </div>
     );
   }
+  
 }
+
 
 
 export default App;
