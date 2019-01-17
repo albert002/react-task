15d5059453c4c460197d66dbd38480609afd2ec7
Albert <albert.hovhannisyan002@gmail.com>
2019-01-17 22:02:57 +0400

    added function for entering multiply keywords added group buttons

--- src/App.js
+++ src/App.js
@@ -5,25 +5,36 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      pictures:[]
+      pictures:[],
+      buttonGroupes:[]
     }
     //Getting images from flickr api
     var self = this;
     this.handleClick = function(){
-      fetch(  'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=f86d456db87838e53eea443b31630f4b&tags='+document.getElementsByClassName("searchInput")[0].value +'&per_page=5&page=1&format=json&nojsoncallback=1'
-      )
+      var picArray = [];
+      var picNames = [];
+      var handleInput = document.getElementsByClassName("searchInput")[0].value.split(" ").length;
+      for(let i = 0; i < handleInput;++i){
+        fetch(  'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=f86d456db87838e53eea443b31630f4b&tags='+document.getElementsByClassName("searchInput")[0].value.split(" ")[i] +'&per_page=5&page=1&format=json&nojsoncallback=1'
+      )    
       .then(function(response){
         return response.json();
       })
       .then(function(j){
-        let picArray = j.photos.photo.map((pic)=>{
+        picArray.push(j.photos.photo.map((pic)=>{
           var picPath = `https://farm${pic.farm}.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}.jpg`;
           return(
-            <img alt="flick_img" src={picPath} className="image"/>
+            <img alt="flick_img" src={picPath} className="image" draggable="true"/>
           )
-        })
-        self.setState({pictures:picArray})
+        }));
+        picNames.push(document.getElementsByClassName("searchInput")[0].value.split(" ").map((name)=>{
+          return(
+            <input type="button" value={name} className="buttonGroupes"/>
+          )
+        }))
+        self.setState({pictures:picArray,buttonGroupes:picNames})
       })
+      }
     }
   }
     
@@ -41,7 +52,9 @@ class App extends Component {
           </div>
 
           <div className="groupChoose">
-            
+          {/* why when i remove this line of code buttons multipies? */}
+           {console.log()} 
+           {this.state.buttonGroupes}
           </div>
 
           <div className="groupPics">
